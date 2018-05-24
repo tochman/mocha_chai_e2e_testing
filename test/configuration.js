4263c0a5f55c40b5b4d2aff5fee77f550f5adc2c
Raoul DIFFOUO <diraulo@gmail.com>
2018-05-24 17:00:43 +0200

    Create helper methods to interact with browsers

--- test/configuration.js
+++ test/configuration.js
@@ -1,53 +1,56 @@
 const puppeteer = require('puppeteer');
 const { expect } = require('chai');
-const _ = require('lodash');
 
-const globalVariables = _.pick(global, ['browser', 'expect']);
+class BrowserHelpers {
+  constructor() { }
 
-const fill_in = async (page, element, value) => {
-  this.inputElement = await page.$(element);
-  await this.inputElement.type(value);
+  async init() {
+    await this.newBrowser()
+    await this.newPage()
+  }
+
+  async newBrowser() {
+    const options = {
+      headless: false,
+      slowMo: 50,
+      timeout: 10000
+    };
+
+    this.browser = await puppeteer.launch(options)
+  }
+
+  async newPage() {
+    this.page = await this.browser.newPage()
+  }
+
+  async visitPage(url) {
+    return await this.page.goto(url)
+  }
+
+  async close() {
+    await this.browser.close()
+  }
+
+  async fillIn(selector, content) {
+    await this.page.waitForSelector(selector)
+    this.inputElement = await this.page.$(selector)
+    await this.inputElement.type(content)
+  }
+
+  async clickOnButton(selector) {
+    await this.page.waitForSelector(selector)
+    await this.page.click(selector)
+  }
+
+  async getContent(element) {
+    const requestedContent = await this.page.$eval(element, requestedContent => requestedContent.textContent);
+    return requestedContent;
+  }
+
+  async getElement(element) {
+    const requestedElement = await this.page.$$(element)
+    return requestedElement;
+  }
 }
 
-const click = async (page, element) => {
-  this.button = await page.$(element);
-  await this.button.click();
-}
-
-const getContent = async (page, element) => {
-  const requestedContent = page.$eval(element, requestedContent => requestedContent.textContent);
-  return requestedContent;
-}
-
-const getElement = async (page, element) => {
-  const requestedElement = await page.$$(element)
-  return requestedElement;
-}
-
-// puppeteer options
-const opts = {
-  headless: false,
-  slowMo: 50,
-  timeout: 10000
-};
-
-// expose variables
-before (async function () {
-  global.expect = expect;
-  global.browser = await puppeteer.launch(opts);
-  global.fill_in = fill_in;
-  global.click = click;
-  global.getContent = getContent;
-  global.getElement = getElement;
-});
-
-// close browser and reset global variables
-after (function () {
-  browser.close();
-
-  global.browser = globalVariables.browser;
-  global.expect = globalVariables.expect;
-});
-
-
-
+module.exports = BrowserHelpers
