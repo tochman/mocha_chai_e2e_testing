61e1c6b6cd42e93e107af920a53bc389229a76c5
tochman <thomas@craftacademy.se>
2018-05-24 11:20:37 +0200

    adds functions for fill_in, click and getContent

--- test/sample.spec.js
+++ test/sample.spec.js
@@ -19,29 +19,22 @@ describe('sample UI test', function () {
       let heading;
   
       await page.waitFor(HEADING_SELECTOR);
-      heading = await page.$eval(HEADING_SELECTOR, heading => heading.innerText);
-  
+      //heading = await page.$eval(HEADING_SELECTOR, heading => heading.innerText);
+      heading = await getContent(page, HEADING_SELECTOR)
       expect(heading).to.eql('Page Title');
     });
   
     it('should have a single content section', async function () {
       const BODY_SELECTOR = '.main-content';
-  
       await page.waitFor(BODY_SELECTOR);
-  
       expect(await page.$$(BODY_SELECTOR)).to.have.lengthOf(1);
     });
 
     it('should display hello message', async function () {
-        const FORM_INPUT = "input[name='name']";
-        const BUTTON = "input[value='Click me']";
-        //await page.waitForSelector(FORM_INPUT);
-        this.inputElement = await page.$(FORM_INPUT);
-        this.button = await page.$(BUTTON);
-        await this.inputElement.type('Thomas');
-        await this.button.click();
-        let content = await page.$eval("div[id='output']", content => content.innerText);
-        expect(await page.$$("div[id='output']")).to.have.eql('Thomas');
+        await fill_in(page, "input[name='name']", "Thomas")
+        await click(page, "input[value='Click me']")
+        let content = await getContent(page, "div[id='output']")
+        expect(content).to.have.eql('Hello Thomas');
       });
   });
 
